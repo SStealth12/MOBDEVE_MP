7582e0c56c13958c09d02e571ab1683a06c90f4a
Miguel Francis M. Estañol <miguel_estanol@dlsu.edu.ph>
2024-10-17 20:52:26 +0800

    Initial commit

---
+++ app/src/main/java/com/mobdeve/s13/estanol/miguelfrancis/mp/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.mobdeve.s13.estanol.miguelfrancis.mp.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/mobdeve/s13/estanol/miguelfrancis/mp/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.mobdeve.s13.estanol.miguelfrancis.mp.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
